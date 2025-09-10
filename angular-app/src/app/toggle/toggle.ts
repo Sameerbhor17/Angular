c00c3014e958c31d81cec181e9aef0c96172fc22
Sameer Bhor <115771003+Sameerbhor17@users.noreply.github.com>
2025-09-10 18:40:52 +0000

    Implement the Traffic Signal Pole using @Switch Directive in Angular

--- angular-app/src/app/toggle/toggle.ts
+++ angular-app/src/app/toggle/toggle.ts
@@ -1,15 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'toggle',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './toggle.html',
   styleUrl: './toggle.scss'
 })
 export class Toggle {
-  public display = true;
+  public display: boolean = true;
+  public show: boolean = true;
+  public signalindicator: string = '';
 
   toggle() {
     this.display = !this.display;
+  }
+
+  trafficSignal(event: string) {
+    this.signalindicator = event;
   }
 }
