6cd72b2f54f79fa14fe1ffb91e47c63054f19323
Sameer Bhor <115771003+Sameerbhor17@users.noreply.github.com>
2025-09-11 17:11:41 +0000

    @For Loop Directive

--- angular-app/src/app/toggle/toggle.ts
+++ angular-app/src/app/toggle/toggle.ts
@@ -19,4 +19,13 @@ export class Toggle {
   trafficSignal(event: string) {
     this.signalindicator = event;
   }
+
+  public userList = ["Onkar", "Abhijit", "Mahesh", "Ashitosh", "Suraj"];
+  public employeeDetails = [
+    {name: "Samruddhi", age: 22, salary: 10000},
+    {name: "Sameer", age: 23, salary: 10000},
+    {name: "Pratik", age: 26, salary: 30000},
+    {name: "Shivani", age: 23, salary: 15000},
+    {name: "Vaibhavi", age: 22, salary: 10000},
+  ]
 }
