d706c087648cb1ec95f7913719d70aeb14413784
Sameer Bhor <115771003+Sameerbhor17@users.noreply.github.com>
2025-09-27 06:39:02 +0000

    Template Driven Forms (Validators Property) in Angular

--- angular-app/src/app/angular-forms/template-driven-forms/template-driven-forms.ts
+++ angular-app/src/app/angular-forms/template-driven-forms/template-driven-forms.ts
@@ -1,14 +1,17 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-forms',
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './template-driven-forms.html',
   styleUrl: './template-driven-forms.scss'
 })
 export class TemplateDrivenForms {
   public formData: any = null;
+  public sendData: boolean = false;
+  
   submitInfo(val: NgForm) {
     console.log(val);
     this.formData = val;
